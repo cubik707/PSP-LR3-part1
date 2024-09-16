fb6e81161493f9b19ef90f331b5145c8c236889e
Vladislava <stdemidovez@gmail.com>
2024-09-16 19:55:37 +0300

    class Plate

---
+++ src/Plate.java
@@ -0,0 +1,25 @@
+public class Plate extends Dishes {
+    private double diameter;
+
+    public Plate(String manufacturer, double price, String style, double diameter) {
+        super(manufacturer, price, style);
+        this.diameter = diameter;
+    }
+
+    public Plate(String manufacturer, double price, double diameter) {
+        this(manufacturer, price, "Неизвестно", diameter);
+    }
+
+    public Plate() {
+        this("Неизвестно", 0.0, "Неизвестно", 0.0);
+    }
+
+    public double getDiameter() {
+        return diameter;
+    }
+
+    @Override
+    public String getName() {
+        return "Тарелка";
+    }
+}
