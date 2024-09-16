5fcdfdc23f38d89a031eab8624eaf557df50c383
Vladislava <stdemidovez@gmail.com>
2024-09-16 19:52:07 +0300

    class Pan

---
+++ src/Pan.java
@@ -0,0 +1,31 @@
+public class Pan extends Dishes {
+    private double handleLength;
+    private String coating;
+
+    public Pan(String manufacturer, double price, String style, double handleLength, String coating) {
+        super(manufacturer, price, style);
+        this.handleLength = handleLength;
+        this.coating = coating;
+    }
+
+    public Pan(String manufacturer, double price, double handleLength) {
+        this(manufacturer, price, "Неизвестно", handleLength, "Нет покрытия");
+    }
+
+    public Pan() {
+        this("Неизвестно", 0.0, "Неизвестно", 0.0, "Нет покрытия");
+    }
+
+    public double getHandleLength() {
+        return handleLength;
+    }
+
+    public String getCoating() {
+        return coating;
+    }
+
+    @Override
+    public String getName() {
+        return "Сковорода";
+    }
+}
