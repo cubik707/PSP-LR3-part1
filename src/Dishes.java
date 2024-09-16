4137a6483996e1cb39239cce553ae9568916074e
Vladislava <stdemidovez@gmail.com>
2024-09-16 19:49:41 +0300

    class Dishes + Pot

---
+++ src/Dishes.java
@@ -0,0 +1,45 @@
+public abstract class Dishes {
+    protected String manufacturer;
+    protected double price;
+    protected String style;
+
+    public Dishes(String manufacturer, double price, String style) {
+        this.manufacturer = manufacturer;
+        this.price = price;
+        this.style = style;
+    }
+
+    public Dishes(String manufacturer, double price) {
+        this(manufacturer, price, "Неизвестно");
+    }
+
+    public Dishes() {
+        this("Неизвестно", 0.0, "Неизвестно");
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public abstract String getName();
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+}
---
+++ src/Pot.java
@@ -0,0 +1,31 @@
+public class Pot extends Dishes {
+    private double volume;
+    private boolean hasLid;
+
+    public Pot(String manufacturer, double price, String style, double volume, boolean hasLid) {
+        super(manufacturer, price, style);
+        this.volume = volume;
+        this.hasLid = hasLid;
+    }
+
+    public Pot(String manufacturer, double price, double volume) {
+        this(manufacturer, price, "Неизвестно", volume, false);
+    }
+
+    public Pot() {
+        this("Неизвестно", 0.0, "Неизвестно", 0.0, false);
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public boolean hasLid() {
+        return hasLid;
+    }
+
+    @Override
+    public String getName() {
+        return "Pot";
+    }
+}
